458c5d58da249dc654ee972216bbf573c72d2221
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2022-11-05 14:48:34 +0200

    Basic index page

--- index.php
+++ index.php
@@ -105,7 +105,77 @@ if (!$dbc)
     </nav>
     <br>
 
-    <h1>Pagrindinis</h1>
+    <!-- Image carousel -->
+    <div class="container-sm w-50">
+        <div id="indicators" class="carousel slide" data-bs-ride="true">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#indicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#indicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#indicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/img/carousel1.png" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/carousel2.png" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/carousel3.png" class="d-block w-100">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#indicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#indicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+    <br>
+
+    <!-- Package tracking and 1 other -->
+    <div class="container w-50">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <div class="container-sm">
+                            <!-- Package tracking form -->
+                            <form action="sekimas.php" method="post">
+                                <div class="mb-3">
+                                    <label for="package_id" class="form-label">Įveskite siuntos numerį:</label>
+                                    <input type="text" class="form-control" id="packageID" name="packageID" placeholder="Siuntos numeris">
+                                </div>
+                                <input type='submit' name='track' class='btn btn-primary' value='Sekti'>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <div class="container-sm">
+                            <!-- Package tracking form -->
+                            <form action="sekimas.php" method="post">
+                                <div class="mb-3">
+                                    <label for="package_id" class="form-label">Įveskite siuntos numerį:</label>
+                                    <input type="text" class="form-control" id="packageID" name="packageID" placeholder="Siuntos numeris">
+                                </div>
+                                <input type='submit' name='track' class='btn btn-primary' value='Sekti'>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
     <?php
