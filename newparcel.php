5a68eae13c359d559ca07b189430c9f297d721e1
emidru1 <101561666+emidru1@users.noreply.github.com>
2022-11-05 16:38:00 +0200

    Added index pages and nav bar

---
+++ newparcel.php
@@ -0,0 +1,115 @@
+<?php
+$server = "localhost";
+$db = "siuntu_tarnyba";
+$user = "root";
+$password = "";
+// prisijungimas prie DB
+$dbc=mysqli_connect($server,$user,$password,$db);
+
+session_start();
+
+# Atsijungimas
+if (isset($_POST["logout"]))
+{
+    setcookie(session_name(), '', 100);
+    session_unset();
+    session_destroy();
+    $_SESSION = array();
+    header('Location: index.php');
+}
+
+if (!isset($_SESSION["username"]))
+{
+    $_SESSION["logged_in"] = 0;
+    $_SESSION["access_level"] = 1;
+}
+else
+{
+    $_SESSION["logged_in"] = 1;
+    $user_id = $_SESSION["id"];
+    $access_level = $_SESSION["access_level"];
+}
+
+if (!$dbc)
+{
+    die ("Nepavyko prisijungti prie duomenų bazės:" .mysqli_error($dbc));
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <?php
+        include_once "includes/header.php";
+        echo getHeader("Pagrindinis");
+    ?>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                  <a class="nav-link" href="#">Pagrindinis</a>
+                  </li>
+
+                  <li class='nav-item'>
+                      <a class='nav-link' href='terminals.php'>Paštomatai</a>
+                  </li>
+
+                  <li class='nav-item'>
+                      <a class='nav-link' href='question.php'>Klauskite</a>
+                  </li>
+                  <li class='nav-item'>
+                      <a class='nav-link active' aria-current="page" href='newparcel.php'>Siuntos registravimas</a>
+                  </li>
+
+                  <?php
+                  # MENU
+                  if ($_SESSION["access_level"] == 2) # Kurjeris
+                  {
+                      echo
+                      "<li class='nav-item'>
+                          <a class='nav-link' href='courier.php'>Kurjeris</a>
+                      </li>";
+                  }
+                  if ($_SESSION["access_level"] == 3) # Administratorius
+                  {
+                    echo
+                    "<li class='nav-item'>
+                    <a class='nav-link' href=''>[ADMIN] 1</a>
+                    </li>";
+                  }
+
+                  if (isset($_SESSION["username"]))
+                  {
+                      $username = $_SESSION["username"];
+                      echo
+                      "</ul>
+                      <span class='navbar-text'>Esate prisijungę kaip $username</span>
+                      <form class='d-flex' method='post'>
+                        <input type='submit' name='logout' class='btn btn-outline-success' value='Atsijungti'>
+                      </form>";
+                  }
+                  else
+                  {
+                        echo
+                        "</ul>
+                        <form class='d-flex'>
+                            <a class='btn btn-outline-success' href='login.php'>Prisijungti</a>
+                        </form>";
+                  }
+        ?>
+        </div>
+    </nav>
+    <br>
+
+    <?php
+        include_once "includes/footer.html";
+    ?>
+</body>
+</html>
