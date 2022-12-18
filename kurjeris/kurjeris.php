762687087da231a86312faf9cd2de365d0374c73
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2022-12-19 00:00:20 +0200

    Show delivery signature in tracking

--- kurjeris/kurjeris.php
+++ kurjeris/kurjeris.php
@@ -61,7 +61,8 @@ if(isset($_POST['select_packages']))
     $packageids=$_POST['selected_packages'];
 	
 for($i=0;$i<$count;$i++){
-    $sql1="UPDATE packages SET courier_id='".$_SESSION['id']."', status='Siunta išvežta pristatymui' WHERE id='" . $packageids[$i] . "'";
+    $date = date('Y-m-d H:i:s', strtotime('+2 hours'));
+    $sql1="UPDATE packages SET courier_id='".$_SESSION['id']."', status='Siunta išvežta pristatymui', planned_delivery_date='$date' WHERE id='" . $packageids[$i] . "'";
     $result1=mysqli_query($dbc, $sql1);
     header('location: kurjeris.php');
     }
@@ -239,7 +240,7 @@ for($i=0;$i<$count;$i++){
     <?php
     if(isset($_SESSION['show_map']))
     {
-        echo "<div class='container' id='map'>
+        echo "<br><div class='container' id='map'>
         <iframe id='iframe' src='https://storage.googleapis.com/maps-solutions-pg19vlu2ft/locator-plus/aoi4/locator-plus.html' width='100%' height='450' style='border:0;' loading='lazy'>
         </iframe>
     </div>";
--- kurjeris/pristatymo_patvirtinimas.php
+++ kurjeris/pristatymo_patvirtinimas.php
@@ -30,6 +30,39 @@ else
     $access_level = $_SESSION["access_level"];
 }
 
+if (isset($_POST["confirm_package"]))
+{
+    $package_id = $_POST["package_id"];
+
+    $_SESSION["package_id"] = $package_id;
+
+    unset($_SESSION["confirm_package"]);
+}
+
+if (isset($_POST["confirm_signature"]))
+{
+    // Save the signature to the database
+    $signature_data = $_POST["signature_data"];
+    $package_id = $_SESSION["package_id"];
+
+    $sql = "UPDATE packages SET status = 'Siunta pristatyta', planned_delivery_date = NOW(), delivery_date = NOW(), signature = '$signature_data' WHERE id = $package_id";
+    $result = mysqli_query($dbc, $sql);
+
+    if ($result)
+    {
+        $_SESSION["notification_message"] = "Siuntos $package_id pristatymas patvirtintas sėkmingai.";
+        $_SESSION["notification_status"] = 1;
+    }
+    else
+    {
+        $_SESSION["notification_message"] = "Siuntos numeriu $package_id pristatymo patvirtinimas nepavyko. Bandykite vėl.";
+        $_SESSION["notification_status"] = 0;
+    }
+    unset($_SESSION["package_id"]);
+    unset($_SESSION["confirm_signature"]);
+    unset($_SESSION["signature_data"]);
+}
+
 if (!$dbc)
 {
     die ("Nepavyko prisijungti prie duomenų bazės:" .mysqli_error($dbc));
@@ -131,14 +164,80 @@ if (!$dbc)
         </div>
 
         <?php
-        // Create a table of packages that are assigned to the courier (courier_id = $user_id)
+            // Show the notification message from session
+            if (isset($_SESSION["notification_message"]))
+            {
+                $message = $_SESSION["notification_message"];
+                if ($_SESSION["notification_status"] == 1)
+                {
+                    echo "<div class='alert alert-success' role='alert'>$message</div>";
+                }
+                else
+                {
+                    echo "<div class='alert alert-danger' role='alert'>$message</div>";
+                }
+                unset($_SESSION["notification_message"]);
+                unset($_SESSION["notification_status"]);
+            }
+
+            if (isset($_SESSION["package_id"]))
+            {
+                $package_id = $_SESSION["package_id"];
+                echo "<div class='container text-center'>
+                <form method='post'>
+                     <input type='hidden' id='signature_data' name='signature_data'>
+                    <canvas id='signature-canvas' width='400' height='200' style='border: 1px solid black'></canvas>
+                    
+                    <script>
+                        // get reference to the canvas element
+                        var canvas = document.getElementById('signature-canvas');
+                      
+                        // get reference to the canvas context, which we will use to draw on the canvas
+                        var ctx = canvas.getContext('2d');
+                        
+                        // set stroke style and line width for the signature
+                        ctx.strokeStyle = 'black';
+                        ctx.lineWidth = 2;
+                        
+                        // create a flag to track whether the user is currently drawing or not
+                        var isDrawing = false;
+                        
+                        // handle mouse down event
+                        canvas.addEventListener('mousedown', function(event) {
+                        isDrawing = true;
+                        ctx.moveTo(event.offsetX, event.offsetY);
+                        });
+                        
+                        var signatureData = canvas.toDataURL();
+                        // handle mouse move event
+                        canvas.addEventListener('mousemove', function(event) {
+                        if (isDrawing) {
+                          ctx.lineTo(event.offsetX, event.offsetY);
+                          ctx.stroke();                        
+                          signatureData = canvas.toDataURL();
+                        }
+                        });
+                        
+                        // handle mouse up event
+                        canvas.addEventListener('mouseup', function(event) {
+                        isDrawing = false;
+                        document.getElementById('signature_data').value = signatureData;
+                        });
+                    </script>
+                  <br><br>
+                  <p>Siuntos numeris: $package_id</p>
+                 <input type='submit' name='confirm_signature' class='btn btn-success w-25' value='Patvirtinti'>
+                 <br><br>
+                </form><hr>
+               </div>";
+        }
         $user_id = $_SESSION["id"];
-        $query = "SELECT * FROM packages WHERE courier_id = $user_id";
+        $query = "SELECT * FROM packages WHERE courier_id = '$user_id' AND status != 'Siunta pristatyta' ORDER BY id DESC";
         $result = mysqli_query($dbc, $query);
 
         echo "<div class='container'>
                     <div class='col-12'>
-                        <table class='table table-striped'>
+                        <form method='post'><table class='table table-striped'>
                             <thead>
                                 <tr>    
                                     <th scope='col'>Siuntos numeris</th>
@@ -161,15 +260,13 @@ if (!$dbc)
                                         <td>$delivery_address</td>
                                         <td>$delivery_date</td>
                                         <td>
-                                            <form method='post'>
-                                                <input type='hidden' name='package_id' value='$package_id'>
-                                                <input type='submit' name='confirm' class='btn btn-success' value='Patvirtinti'>
-                                            </form>
+                                            <input type='hidden' name='package_id' value='$package_id'>
+                                            <input type='submit' name='confirm_package' class='btn btn-success' value='Pristatyti'>
                                         </td>
                                     </tr>";
                             }
                             echo "</tbody>
-                        </table>
+                        </table></form>
                     </div>
                 </div>
             </div>";
